fd5e2805fb382fed2f912da3b95675b943065de7
David Daugherty <david.m.daugherty@gmail.com>
2018-06-25 20:18:25 -0600

    Add css and html for color cards and header

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+app.use(express.static('public'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Chat Box';
