5d09bcd3af5033eceb23f16af2c5fbd8a953f724
David Daugherty <david.m.daugherty@gmail.com>
2018-06-27 14:53:36 -0600

    Hook up database

--- db/migrations/20180627130437_initial.js
+++ db/migrations/20180627130437_initial.js
@@ -10,6 +10,11 @@ exports.up = function(knex, Promise) {
     knex.schema.createTable('palettes', function(table) {
       table.increments('id').primary();
       table.string('name');
+      table.string('colorOne');
+      table.string('colorTwo');
+      table.string('colorThree');
+      table.string('colorFour');
+      table.string('colorFive');
       table.integer('project_id').unsigned()
       table.foreign('project_id').references('projects.id');
 
