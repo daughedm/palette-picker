603b4f46eb7a8af9bd077c030bc98a073b6eb891
David Daugherty <david.m.daugherty@gmail.com>
2018-06-26 18:09:01 -0600

    Add start of locking function

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,6 @@
-
+window.onload = () => {
+  
+}
 
 const updatePalette = () => {
   const palette = [$('.color-one'), $('.color-two'), $('.color-three'), $('.color-four'), $('.color-five')];
@@ -10,9 +12,23 @@ const updatePalette = () => {
   })
 }
 
-
 const colorGenerator = () => {
   return '#' + (Math.random() * 0xFFFFFF << 0).toString(16).toUpperCase();
 }
 
+$(document).ready(function () {
+  var toggle = 0;
+  $("div .color").click(function () {
+    if (toggle == 0) {
+      $(".lock").attr("src", "assets/unlocked.svg");
+      toggle = 1;
+    }
+    else if (toggle == 1) {
+      $(".lock").attr("src", "assets/locked.svg");
+      toggle = 0;
+    }
+  });
+});
+
 $('.generate-btn').on('click', updatePalette);
+$()
